965ec7c876ac22614bec243017d0a78ab0be43cf
Adrian Soim <adisoim2002@gmail.com>
2023-12-10 20:43:26 +0200

    am terminat cu partenerii da sa verifici si tu ca io s retardoid cateodata + CRECA TRE SA FACEM LAYOUT NOU PT PAGINILE IN CARE VDM SPEAKERI ETC:)))

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,6 +38,11 @@
                             Speakeri
                         </a>
 
+                        <a href="{{ route('partners.index') }}"
+                           class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            Parteneri
+                        </a>
+
                         <!-- Dropdown Content -->
                         <div style="top:57px" class=" absolute hidden group-hover:block z-50 mt-2 w-48 bg-white shadow-lg rounded-md py-1">
                             <a href="{{ route('sponsors.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sponsori</a>
