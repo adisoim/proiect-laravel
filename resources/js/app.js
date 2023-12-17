cb879758fe594751eae7bf2bc140adf2e9928069
Adrian Soim <adisoim2002@gmail.com>
2023-12-17 18:01:04 +0200

    user

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,6 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
-require('flatpickr/dist/flatpickr.css');
-window.flatpickr = require('flatpickr');
-
 window.Alpine = Alpine;
 
 Alpine.start();
